14a69c8b050bd8194e7029538e7785955300d494
svivek <svivek@extremenetworks.com>
2020-10-09 00:13:58 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/vivek-india/godhs/bp
+
+go 1.12
+
+require github.com/sirupsen/logrus v1.7.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	logrus.Infoln("Hello World")
+}
